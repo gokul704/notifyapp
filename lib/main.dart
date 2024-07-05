f37de8a3ef4e682ab8b0c20ce774547218693d8a
Gokul <mamandurugokul1@gmail.com>
2024-07-05 09:02:11 +0530

    Added notifications package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:notifications/notifications.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Noftification Storer'),
     );
   }
 }
@@ -56,6 +59,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Notifications _notifications = Notifications();
+  late StreamSubscription<NotificationEvent> _subscription;
+  List<NotificationEvent> _log = [];
+  bool started = false;
 
   void _incrementCounter() {
     setState(() {
@@ -69,57 +76,67 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    startListening();
+  }
+
+  void onData(NotificationEvent event) {
+    setState(() {
+      _log.add(event);
+    });
+    print(event.toString());
+  }
+
+  void example() {
+    Notifications().notificationStream!.listen((event) => print(event));
+  }
+
+  void startListening() {
+    _notifications = Notifications();
+    try {
+      _subscription = _notifications!.notificationStream!.listen(onData);
+      setState(() => started = true);
+    } on NotificationException catch (exception) {
+      print(exception);
+    }
+  }
+
+  void stopListening() {
+    _subscription?.cancel();
+    setState(() => started = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Notifications Example app'),
+        ),
+        body: new Center(
+            child: new ListView.builder(
+                itemCount: _log.length,
+                reverse: true,
+                itemBuilder: (BuildContext context, int idx) {
+                  final entry = _log[idx];
+                  return ListTile(
+                      leading:
+                          Text(entry.timeStamp.toString().substring(0, 19)),
+                      trailing:
+                          Text(entry.packageName.toString().split('.').last));
+                })),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: started ? stopListening : startListening,
+          tooltip: 'Start/Stop sensing',
+          child: started ? Icon(Icons.stop) : Icon(Icons.play_arrow),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
